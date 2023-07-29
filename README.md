861f2e099eed83a2c0a2fa87ca93f9fddf8e855f
HernanGoldy <hernanmdq@gmail.com>
2023-07-29 12:41:20 -0300

    agrego práctica de chapter02 fig02_01

---
+++ README.md
@@ -0,0 +1,7 @@
+## Java - Cómo programar - Décima edición
+
+En este proyecto se transcriben los ejemplos y actividades del libro con la única finalidad de reproducir las prácticas de código Java.
+
+---
+
+![](Front.png)
---
+++ src/chapter02/fig02_01/Welcome1.java
@@ -0,0 +1,13 @@
+package chapter02.fig02_01;
+
+// Programa para imprimir texto por consola
+
+public class Welcome1 {
+
+    // El método «main» empieza la ejecución de la aplicación en Java
+    public static void main(String[] args) {
+
+        System.out.println("Bienvenido a la programacion en Java!");
+
+    } // fin del método «main»
+} // fin de la clase «Welcome1»
