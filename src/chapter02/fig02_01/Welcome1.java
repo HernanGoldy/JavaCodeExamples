b08602d8af4b6ba44d2f79e13bfd820df5e841a1
HernanGoldy <hernanmdq@gmail.com>
2023-07-31 09:02:15 -0300

    agrego práctica de chapter02 fig02_02

--- src/chapter02/fig02_01/Welcome1.java
+++ src/chapter02/fig02_01/Welcome1.java
@@ -1,10 +1,10 @@
 package chapter02.fig02_01;
 
-// Programa para imprimir texto por consola
+// Programa para imprimir texto por consola.
 
 public class Welcome1 {
 
-    // El método «main» empieza la ejecución de la aplicación en Java
+    // El método main empieza la ejecución de la aplicación en Java
     public static void main(String[] args) {
 
         System.out.println("Bienvenido a la programacion en Java!");
---
+++ src/chapter02/fig02_02/Welcome2.java
@@ -0,0 +1,13 @@
+package chapter02.fig02_02;
+
+// Imprimir una línea de texto con varias instrucciones.
+
+public class Welcome2 {
+
+    // El método main empieza la ejecución de la aplicación en Java
+    public static void main(String[] args) {
+
+        System.out.print("Bienvenido a ");
+        System.out.println("la programacion en Java!");
+    } // fin del método main
+} // fin de la clase Welcome2
