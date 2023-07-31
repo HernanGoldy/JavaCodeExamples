feecc4764dbe213fbf491e24f65d21d93ce60f25
HernanGoldy <hernanmdq@gmail.com>
2023-07-31 10:56:46 -0300

    agrego práctica de chapter02 fig02_06

---
+++ src/chapter02/fig02_06/Welcome4.java
@@ -0,0 +1,13 @@
+package chapter02.fig02_06;
+
+// Imprimir varias líneas con el método System.ouy.printf.
+
+public class Welcome4 {
+
+    // el método main empieza la ejecución de la aplicación de Java
+    public static void main(String[] args) {
+
+        System.out.printf("%s%n%s%n",
+                "Bienvenido a", "la programacion en Java!");
+    } // fin del método main
+} // fin de la clase Welcome4
