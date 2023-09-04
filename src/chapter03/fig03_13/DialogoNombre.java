86cc9f070531c86074e8946db8a2b2f833e3ce07
HernanGoldy <hernanmdq@gmail.com>
2023-09-04 05:53:48 -0300

    agrego práctica de chapter03 - fig03_13

--- src/chapter03/fig03_13/DialogoNombre.java
+++ src/chapter03/fig03_13/DialogoNombre.java
@@ -1,4 +1,20 @@
 package chapter03.fig03_13;
 
+// Fig. 3.13: DialogoNombre.java
+// Entrada básica con un cuadro de diálogo
+
+import javax.swing.*;
+
 public class DialogoNombre {
+
+    public static void main(String[] args) {
+        // pide al usuario que escriba su nombre
+        String nombre = JOptionPane.showInputDialog("Cual es su nombre?");
+
+        // cea el mensaje
+        String mensaje = String.format("Bienvenido %s, a la progrmación en Java!", nombre);
+
+        // muestra el mensaje
+        JOptionPane.showMessageDialog(null, mensaje);
+    }
 }
