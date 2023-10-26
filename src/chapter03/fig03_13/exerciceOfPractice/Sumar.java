34268d788e613cc356971af050f78f8e664e2a08
HernanGoldy <hernanmdq@gmail.com>
2023-10-26 12:55:45 -0300

    modifico ejercicio de práctica de chapter03

--- src/chapter03/fig03_13/exerciceOfPractice/Sumar.java
+++ src/chapter03/fig03_13/exerciceOfPractice/Sumar.java
@@ -1,10 +1,11 @@
 package chapter03.fig03_13.exerciceOfPractice;
 
 /*
-Modifique el programa de suma que aparece en la figura 2.7 para usar la entrada y salida en un cuadro de diálogo con los métodos de la clase JOptionPane.
-Como el método showInputDialog devuelve un objeto String, debe convertir el objeto String que introduce el usuario a un int para utilizarlo en los cálculos.
-El método parseInt es un método static de la clase Integer (del paquete java.lang) que recibe un argumento String que representa a un entero y devuelve el valor como int.
-Si el objeto String no contiene un entero válido, el programa terminará con un error.
+Modifique el programa de suma que aparece en la figura 2.7 para usar la entrada y salida en un cuadro de diálogo con
+los métodos de la clase JOptionPane. Como el método showInputDialog devuelve un objeto String, debe convertir el
+objeto String que introduce el usuario a un int para utilizarlo en los cálculos. El método parseInt es un método
+static dela clase Integer (del paquete java.lang) que recibe un argumento String que representa a un entero y
+devuelve el valor como int. Si el objeto String no contiene un entero válido, el programa terminará con un error.
  */
 
 import javax.swing.*;
