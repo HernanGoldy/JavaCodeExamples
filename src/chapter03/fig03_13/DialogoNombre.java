1b3dd9c3722fbd55ee750fbcf9fc0071a4742833
HernanGoldy <hernanmdq@gmail.com>
2023-10-26 12:53:20 -0300

    modifico ejercicio de práctica de chapter03

--- src/chapter03/fig03_13/DialogoNombre.java
+++ src/chapter03/fig03_13/DialogoNombre.java
@@ -8,11 +8,12 @@ import javax.swing.*;
 public class DialogoNombre {
 
     public static void main(String[] args) {
-        // pide al usuario que escriba su nombre
-        String nombre = JOptionPane.showInputDialog("Cual es su nombre?");
 
-        // cea el mensaje
-        String mensaje = String.format("Bienvenido %s, a la progrmación en Java!", nombre);
+        // pide al usuario que escriba su nombre
+        String nombre = JOptionPane.showInputDialog("¿Cual es su nombre?");
+
+        // crea el mensaje
+        String mensaje = String.format("¡Bienvenido %s, a la progrmación en Java!", nombre);
 
         // muestra el mensaje
         JOptionPane.showMessageDialog(null, mensaje);
